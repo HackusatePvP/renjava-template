9e5b7cd0c3f3f850fc81721a4de6bd1fa32642aa
HackusatePvP <supermftw@gmail.com>
2025-02-15 21:07:58 -0600

    Updated to support the latest RenJava release.

--- src/main/java/template/renjava/Main.java
+++ src/main/java/template/renjava/Main.java
@@ -1,17 +1,22 @@
 package template.renjava;
 
 import me.piitex.renjava.RenJava;
-import me.piitex.renjava.api.Game;
 import me.piitex.renjava.configuration.Configuration;
+import me.piitex.renjava.configuration.Game;
 import me.piitex.renjava.gui.Window;
 
 @Game(name = "Template Project", author = "Unknown", version = "0.0.0")
 @Configuration(title = "{name} v{version}", width = 1920, height = 1080)
 public class Main extends RenJava {
+    private static Main instance;
+
+    public Main() {
+        instance = this;
+    }
 
     @Override
     public void preEnabled() {
-
+        setMainMenu(new GameMenu());
     }
 
     @Override
